d51c1cec2827ac5004df46bb7f128f698e3a2593
Jordan <iraborjordan1@gmail.com>
2020-06-19 17:49:09 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## quick-start-gatsby-template
+
+I spend a lot of time configuring a fresh Gatsby project to include testing, SASS support, typescript and prettier configurations. This boiler plate contains a freshly installed Gatsby project that has all of these functionalities including some utility styles and functions I use to keep my code responsive.
+
+## Running in development
+
+```
+yarn
+
+yarn test
+
+yarn start
+```
---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteMetadata: {
+    title: `Boilerplate`,
+    description: `Get coverage for Medicare deductibles, co-pays, and other out-of-pocket expenses.`,
+    author: `@gatsbyjs`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+        isTSX: true,
+        jsxPragma: `jsx`,
+        allExtensions: true
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/logos/logo.png` // This path is relative to the root of the site.
+      }
+    }
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ]
+}
